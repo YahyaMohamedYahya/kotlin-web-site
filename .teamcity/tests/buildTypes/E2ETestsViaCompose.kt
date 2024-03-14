ca8e97ca7010d2382d85836b67bb4a8f6c43be61
berezinant <berezinant@gmail.com>
2024-03-14 14:02:16 +0100

    ci(KTL-1352): added configuration to run e2e test via compose

--- .teamcity/tests/buildTypes/E2ETestsViaCompose.kt
+++ .teamcity/tests/buildTypes/E2ETestsViaCompose.kt
@@ -1,34 +1,30 @@
 package tests.buildTypes
 
-import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.buildFeatures.pullRequests
-import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCompose
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 
-object E2ETests : BuildType({
-  name = "E2E tests"
+object E2ETestsViaCompose : BuildType({
+  name = "E2E tests via compose"
 
   vcs {
     root(vcsRoots.KotlinLangOrg)
   }
 
-    dependencies {
-        artifacts(AbsoluteId("Kotlin_KotlinSites_KotlinlangTeamcityDsl_BuildReferenceDocs")) {
-            cleanDestination = true
-            artifactRules = "+:docs.zip!** => dist/"
-        }
+  steps {
+    script {
+      scriptContent = "docker-compose -f docker-compose-e2e-statics.yml up -d"
     }
 
-  steps {
-//      dockerCompose {
-//          file = "docker-compose-e2e-statics.yml"
-//      }
-      script {
-          scriptContent = "docker compose -f docker-compose-e2e-statics.yml up --exit-code-from playwright"
-      }
+    script {
+      scriptContent = "docker-compose exec playwright yarn run test:visual:ci"
+    }
+
+    script {
+      scriptContent = "docker-compose down"
+    }
   }
 
   requirements {
